50438fd79d7e0431776c0e5210f227a06bedce06
shamy750 <shameeth.suresh@softsuave.org>
2024-06-07 12:18:21 +0530

    Initial commit

---
+++ sampleIosApp/sampleIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleIosAppApp.swift
+//  sampleIosApp
+//
+//  Created by Softsuave on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct sampleIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
